8c5db194dae0791464b3ac002efc38caf1809d2a
zts <247931368@qq.com>
2019-05-05 17:37:35 +0800

    新增：界面布局

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,11 +1,20 @@
-require('./index.less')
+require('./index.css')
 // canvas鼠标滑过效果
 require('./canvas.js')
 
 var page = {
 	init: function(){
-		
-	}
+		this.navClick()
+	},
+	navClick: function () {
+		$('.nav .nav-item').click(function () {
+			$(this).siblings().removeClass('active')
+			$(this).addClass('active')
+			if($(this).text() === '学习笔记') {
+
+			}
+        })
+    }
 }
 
 page.init()
